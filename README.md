4d5287864180dfe26d7f4c979383189b629747ff
dean-shaw <dn.at.shw@gmail.com>
2020-02-27 11:53:58 -0500

    cleaned up and commented code

--- README.md
+++ README.md
@@ -24,51 +24,20 @@ For your solution explain:
 
 
 
-
-Discoveries
-filename format is pageviews-YYYYMMDD-HH0000.gz
-pageviews data format is <domain code> <page title> <count views> <total response size>
-blacklist data format is <domain code> <page title>
-
-Question
-s
-1. Which sub-domains are there? Bullet point 3 says to aggregate to "unique domain". Is "unique domain" the same as "sub domain"? Are both of these equal to the first column of the pageviews file? If that is the case, then are we simply aggregating by the first column and returning the top 25 pages per domain code?
-2. We only look at the pageview files, not the projectview files right
-3. Do we include mobile as the same domain code? 
-	e.g. do https://en.wikipedia.org/wiki/(You_Drive_Me)_Crazy_Tour and 
-		https://en.m.wikipedia.org/wiki/(You_Drive_Me)_Crazy_Tour
-		represented by 
-		en (You_Drive_Me)_Crazy_Tour
-		en.m (You_Drive_Me)_Crazy_Tour
-		point to the same "domain"?
-4. Bullet point 5, "only run if necessary" does that mean do not re-compute if a date+hour has already been computed/saved in the past?
-5. Am I building the data pipeline/application or just describing how we would build it
-6. Am I building the application as something that runs on AWS or runs locally? or is that up to me
-
-
-More questions
-1. Instead of strictly cutting off top 25 for each domain, pages tied for last place will be returned regardless of overrun. For example, if 3 pages are tied for 24th place, all 3 will be returned (thus 26 results). Is this okay?
-
-2. I'm noticing that the page titles in the blacklist have different encodings than the page titles from the raw pageviews file.
-
-for example:
-The german page for Centipede appears as "de Hundertfüßer 2 0" in the pageviews file and "de Hundertf%C3%BC%C3%9Fer" in the blacklist file. 
-
-I can fix this case with a simple url decoding. However, there are cases where straight url decoding will not yield the equivalent value. 
-
-For example, the german page for Hitzacker appears as "de Hitzacker_(Elbe) 1 0" in the pageviews file and "de Hitzacker%20(Elbe)" in the blacklist file. Normally %20 decodes to the empty space character " " instead of underscore.
-
-Should I be decoding the blacklist file before removing equivalent page titles? Should I treat %20 as a bug in the blacklist file and decode them to underscore instead of space, or leave as is?
-
-
-
-
 [Assumptions]
-1. We are treating mobile and web as separate domains. For example the web version of english page for Tokyo, "en Tokyo", will be treated separately from the mobile version of the same page, "en.m Tokyo".
+1. Raw data is located online at "https://dumps.wikimedia.org/other/pageviews/{year}/{year}-{month}/pageviews-{year}{month}{day}-{hour}0000.gz"
 
-2. Instead of strictly cutting off top 25 for each domain, pages tied for last place will be returned regardless of overrun. For example, if 3 pages are tied for 24th place, all 3 will be returned (thus 26 results).
+2. pageviews data format is <domain code> <page title> <count views> <total response size>
 
-3. Page titles in the blacklist have different encodings than the page titles from the raw pageviews file.
+3. blacklist data format is <domain code> <page title>
+
+4. We are returning top 25 page titles, ranked by view count, per domain code
+
+5. We are treating mobile and web as separate domains. For example the web version of english page for Tokyo, "en Tokyo", will be treated separately from the mobile version of the same page, "en.m Tokyo".
+
+6. Instead of strictly cutting off top 25 for each domain, pages tied for last place will be returned regardless of overrun. For example, if 3 pages are tied for 24th place, all 3 will be returned (thus 26 results).
+
+7. Page titles in the blacklist have different encodings than the page titles from the raw pageviews file.
 
 for example:
 The german page for Centipede appears as "de Hundertfüßer 2 0" in the pageviews file and "de Hundertf%C3%BC%C3%9Fer" in the blacklist file. 
--- WikiAggregator.py
+++ WikiAggregator.py
@@ -3,8 +3,9 @@ import platform
 import logging
 import unittest
 from operator import add
+from pyspark import version as pyv
 from pyspark.sql import SparkSession, Window
-from pyspark.sql.functions import udf, concat_ws, stddev_pop, avg, rank, col
+from pyspark.sql.functions import udf, rank, col
 from pyspark.sql.types import IntegerType, StringType, StructType, StructField
 from datetime import datetime, timedelta
 import urllib.parse
@@ -12,7 +13,7 @@ from gzip import decompress
 from requests import get
 from os import path
 
-HOUR = timedelta(hours=3600)
+HOUR = timedelta(hours=1)
 TOP_N = 25
 
 # source data
@@ -29,310 +30,231 @@ count_views = "count_views"
 total_response_size = "total_response_size"
 
 schema = StructType([
-	StructField(domain_code, StringType()),
-	StructField(page_title, StringType()),
-	StructField(count_views, IntegerType()),
-	StructField(total_response_size, IntegerType())
-	])
+    StructField(domain_code, StringType()),
+    StructField(page_title, StringType()),
+    StructField(count_views, IntegerType()),
+    StructField(total_response_size, IntegerType())
+    ])
 
 # blacklist columns and schema
 encoded_page_title = "encoded_page_title"
 blacklist_schema = StructType([
-	StructField(domain_code, StringType()),
-	StructField(encoded_page_title, StringType())
-	])
+    StructField(domain_code, StringType()),
+    StructField(encoded_page_title, StringType())
+    ])
 
 # udfs
-decode_udf = udf(lambda pt: urllib.parse.unquote(pt))
 encode_udf = udf(lambda pt: urllib.parse.quote(pt))
 
 
-
-# raw columns
-pudt = 'tpep_pickup_datetime'
-dodt = 'tpep_dropoff_datetime'
-ratecodeid = 'ratecodeid'
-puid = 'pulocationid'
-doid = 'dolocationid'
-payment_type = 'payment_type'
-total_amount = 'total_amount'
-month_year = 'month_year'
-
-# transformed columns
-duration = 'duration'
-key_pair = 'key_pair'
-
 def lpad(i):
-	s = "0" + str(i)
-	return s[-2:]
+    """
+    left pad integers (specifically day/month/hour) with 0s
 
-def calc_duration(dtstr1, dtstr2):
-	"""
-	dts are of the form YYYY-mm-DD HH:MM:SS
-	e.g. 2019-02-01 00:59:04
-	:param dt1:
-	:param dt2:
-	:return:
-	"""
-	fmt = '%Y-%m-%d %H:%M:%S'
-	dt1 = datetime.strptime(dtstr1, fmt)
-	dt2 = datetime.strptime(dtstr2, fmt)
-	return (dt2-dt1).seconds
+    :param i: integer to pad
+    :return: 2 digit i, left padded with zeroes
+    """
+    s = "0" + str(i)
+    return s[-2:]
 
-
-def add_month_year(dtstr):
-	fmt = '%Y-%m-%d %H:%M:%S'
-	dt = datetime.strptime(dtstr, fmt)
-	return "{}_{}".format(dt.month, dt.year)
-
-
-def transform_and_aggregate(df):
-	"""
-	data is already "filtered" down to month and yellowcab only
-	take columns:
-		tpep_pickup_datetime
-		tpep_dropoff_datetime
-		ratecodeid
-		pulocationid
-		dolocationid
-		payment_type
-		total_amount
-	additional filters to apply:
-		for simplification purposes, we will only consider the following categories
-		ratecodeid - only take 1=standard rate
-		payment_type - only take 1=credit card and 2=cash
-	some data considerations:
-		according to https://www1.nyc.gov/site/tlc/passengers/taxi-fare.page
-		there is a $1 "rush hour" surcharge from 4pm-8pm on weekdays, excluding holidays
-		there is a 50 cents overnight surcharge from 8pm to 6am
-		there is no extra charge for extra customers
-	add column - duration in seconds (tpep_dropoff_datetime - tpep_pickup_datetime)
-	group df by
-		pulocationid
-		dolocationid
-		truncated hour
-	aggregate by
-		avg duration
-		avg total_amount
-	return dataframe of tuples (pulocationid, dolocationid, avg duration, avg total amount)
-
-	:param df: dataframe of yellowcab taxi data
-	:return: df
-	"""
-
-	df = df.select([pudt, dodt, ratecodeid, puid, doid, payment_type, total_amount])
-	df = df.filter(f'{ratecodeid} = 1')
-	df = df.filter(f'{payment_type} = 1 or {payment_type} = 2')
-	calc_duration_udf = udf(calc_duration, returnType=IntegerType())
-	add_month_year_udf = udf(add_month_year)
-	df = df.withColumn(duration, calc_duration_udf(pudt, dodt))
-	df = df.withColumn(month_year, add_month_year_udf(pudt))
-	aggdf = df.groupBy(puid, doid, month_year).agg(avg(duration), avg(total_amount), stddev_pop(duration), stddev_pop(total_amount))
-	aggdf = aggdf.withColumn(key_pair, concat_ws('-', puid, doid, month_year))
-
-	return aggdf
 
 def clean_pageview(pv):
-	print("cleaning pageview")
-	return pv.withColumn(encoded_page_title, encode_udf(page_title))
+    """
+    url decode the raw page title from pageviews
 
-def clean_blacklist(bl):
-	return bl.withColumn(page_title, decode_udf(encoded_page_title))
+    :param pv: dataframe of raw pageview data
+    :return: pageview dataframe with an additional url encoded pagetitle column
+    """
+    print("cleaning pageview")
+    return pv.withColumn(encoded_page_title, encode_udf(page_title))
+
 
 def remove_blacklist(df, bl):
-	print("removing blacklist")
-	joined = df.join(bl, [domain_code, encoded_page_title], 'left_anti')
-	return joined
+    """
+    remove rows from pageview whos page title and domain code are matched in blacklist
+
+    :param df: cleaned pageview dataframe
+    :param bl: blacklist dataframe
+    :return: pageview dataframe with blacklist removed
+    """
+    print("removing blacklist")
+    joined = df.join(bl, [domain_code, encoded_page_title], 'left_anti')
+    return joined
+
 
 def window_and_sort(df, n):
-	print("windowing and sorting")
-	window = Window.partitionBy(df[domain_code]).orderBy(df[count_views].desc())
-	ranked = df.withColumn("rank", rank().over(window)).filter(col('rank') <= n)
-	return ranked
+    """
+    include only top N page titles per domain code.
+    window by domain code and add rank, then filter.
+    note: if several results are tied, they will all be returned
 
-
-def run_batch_old(s3_input_file, elasticache_output_location):
-	"""
-	yellowcab taxi data is approximately 630 mb per file
-	fhv high volume data is approximately 1.2 gb per file
-	we will only concern ourselves with yellowcab data for now
-
-	read yellowcab taxi data file from s3 into dataframe
-		data will be at e.g. s3://stam-dshaw-raw/yellow_tripdata_2019-02.csv
-	apply transformation
-	load to elasticache
-
-	:param s3_input_file:
-	:return:
-	"""
-	spark = SparkSession.builder.appName("TaxiAggregator").getOrCreate()
-	print(f"reading {s3_input_file}")
-	df = spark.read.csv(s3_input_file, header=True)
-	print("aggregating")
-	aggdf = transform_and_aggregate(df)
-	print("showing")
-	aggdf.show(n=10)
-	print("writing to redis")
-	aggdf.write.format("org.apache.spark.sql.redis").option("table", "taxiagg").option("key.column", key_pair).save(mode='overwrite')
-	print("stopping spark")
-	spark.stop()
-	return
+    :param df: pageview dataframe to window and sort
+    :param n: top N to return per domain code
+    :return: pageview dataframe with only top N per domain code
+    """
+    print("windowing and sorting")
+    window = Window.partitionBy(df[domain_code]).orderBy(df[count_views].desc())
+    ranked = df.withColumn("rank", rank().over(window)).filter(col('rank') <= n)
+    return ranked
 
 
 def validate_input(start, end):
-	fmt = '%Y%m%d%H'
-	sdt = datetime.strptime(start, fmt)
-	edt = datetime.strptime(end, fmt)
-	if sdt > edt:
-		raise Exception("start must be before end")
-	return sdt, edt
+    """
+    helper function to ensure valid start and end times are inputted
+
+    :param start: start timestamp
+    :param end: end timestamp
+    :return: True if dates are valid
+    """
+    fmt = '%Y%m%d%H'
+    sdt = datetime.strptime(start, fmt)
+    edt = datetime.strptime(end, fmt)
+    if sdt > edt:
+        raise Exception("start must be before end")
+    return sdt, edt
+
 
 def check_cache(year, month, day, hour):
-	"""
-	return True if results for this timestamp have already been computed
-	"""
-	return path.exists(out_processed.format(n=TOP_N, year=year, month=month, day=day, hour=hour))
+    """
+    check if this result set has already been computed.
+    Results are saved to local disk, so we just check local disk
+
+    :param year:
+    :param month:
+    :param day:
+    :param hour:
+    :return: True if file exists locally
+    """
+    return path.exists(out_processed.format(n=TOP_N, year=year, month=month, day=day, hour=hour))
+
 
 def download_and_gunzip(year, month, day, hour):
-	full_url = src_url.format(year=year, month=month, day=day, hour=hour)
-	print(f"downloading from {full_url}")
-	download_location = in_raw.format(year=year, month=month, day=day, hour=hour)
-	with open(download_location, 'wb') as f:
-		f.write(decompress(get(full_url).content))
-	print(f"done writing to {download_location}")
-	return download_location
+    """
+    download the raw pageview data corresponding to this datetime,decompress, and save to disk
+
+    :param year: 
+    :param month: 
+    :param day: 
+    :param hour: 
+    :return: local filepath to downloaded data
+    """
+    full_url = src_url.format(year=year, month=month, day=day, hour=hour)
+    print(f"downloading from {full_url}")
+    download_location = in_raw.format(year=year, month=month, day=day, hour=hour)
+    with open(download_location, 'wb') as f:
+        f.write(decompress(get(full_url).content))
+    print(f"done writing to {download_location}")
+    return download_location
+
 
 def run_batch(start, end):
-	"""
-	for each date-hour
-		if date-hour not in outputs folder
-			download source datea
-			gunzip
-			transform
-			write to outputs folder
-	"""
-	sdt, edt = validate_input(start, end)
-	spark = SparkSession.builder.appName("WikiAggregator").getOrCreate()
-	bl = spark.read.csv("blacklist", schema=blacklist_schema, sep=" ")
-	while sdt <= edt:
-		month = lpad(sdt.month)
-		day = lpad(sdt.day)
-		hour = lpad(sdt.hour)
-		if not check_cache(sdt.year, month, day, hour):
-			source_data = download_and_gunzip(sdt.year, month, day, hour)
-			pv = spark.read.csv(source_data, schema=schema, sep = " ")
+    """
+    for each date-hour
+        if date-hour has not been computed already
+            download and gunzip source data to disk
+            transform
+            write results to disk
+    :param start: start date and time in YYYYmmddHH format
+    :param end: end date and time in YYYYmmddHH format
+    :return:
+    """
+    sdt, edt = validate_input(start, end)
+    spark = SparkSession.builder.appName("WikiAggregator").getOrCreate()
+    bl = spark.read.csv("blacklist", schema=blacklist_schema, sep=" ")
 
-			clean_pv = clean_pageview(pv)
-			bl_removed = remove_blacklist(clean_pv, bl)
-			transformed = window_and_sort(bl_removed, TOP_N)
-			sort_by_domain_and_rank = transformed.orderBy(domain_code, "rank")
+    while sdt <= edt:
+        month = lpad(sdt.month)
+        day = lpad(sdt.day)
+        hour = lpad(sdt.hour)
+        if not check_cache(sdt.year, month, day, hour):
+            source_data = download_and_gunzip(sdt.year, month, day, hour)
+            pv = spark.read.csv(source_data, schema=schema, sep = " ")
 
-			write_location = out_processed.format(n=TOP_N, year=sdt.year, month=month, day=day, hour=hour)
-			print(f"sinking to {write_location}")
-			sort_by_domain_and_rank.coalesce(1).write.csv(write_location, header=True)
-		else:
-			print(f"{sdt} already processed, skipping...")
-		sdt += HOUR
-	print("all done.")
+            clean_pv = clean_pageview(pv)
+            bl_removed = remove_blacklist(clean_pv, bl)
+            transformed = window_and_sort(bl_removed, TOP_N)
+            sort_by_domain_and_rank = transformed.orderBy(domain_code, "rank")
+
+            write_location = out_processed.format(n=TOP_N, year=sdt.year, month=month, day=day, hour=hour)
+            print(f"sinking to {write_location}")
+            sort_by_domain_and_rank.coalesce(1).write.csv(write_location, header=True)
+        else:
+            print(f"{sdt} already processed, skipping...")
+        sdt += HOUR
+
+    print("stopping spark")
+    spark.stop()
+    print("all done.")
+    return
+
 
 def main():
-	print("############################")
-	print("Welcome to DD2020 Wikipedia Aggregator")
-	print("You are using python version {}".format(platform.python_version()))
-	print("############################")
-	parser = argparse.ArgumentParser(description="Taxi Cost and Time Aggregator")
-	parser.add_argument('--start', dest="start", help="start date and hour in YYYYMMDDHH format", type=str, required=True)
-	parser.add_argument('--end', dest="end", help="end date and hour in YYYYMMDDHH format", type=str, required=True)
-	args = parser.parse_args()
-	run_batch(args.start, args.end)
+    print("######################################################")
+    print("Welcome to DD2020 Wikipedia Aggregator")
+    print("You are using pyspark version {}".format(pyv.__version__))
+    print("On python version {}".format(platform.python_version()))
+    print("######################################################")
+    parser = argparse.ArgumentParser(description="Taxi Cost and Time Aggregator")
+    parser.add_argument('--start', dest="start", help="start date and hour in YYYYMMDDHH format", type=str, required=True)
+    parser.add_argument('--end', dest="end", help="end date and hour in YYYYMMDDHH format", type=str, required=True)
+    args = parser.parse_args()
+    run_batch(args.start, args.end)
 
 
 class PySparkTest(unittest.TestCase):
-	@classmethod
-	def supress_py4j_logging(cls):
-		logger = logging.getLogger('py4j')
-		logger.setLevel(logging.WARN)
+    @classmethod
+    def supress_py4j_logging(cls):
+        logger = logging.getLogger('py4j')
+        logger.setLevel(logging.WARN)
 
-	@classmethod
-	def create_testing_pyspark_session(cls):
-		return SparkSession.builder.master('local').appName('unittest-pyspark').enableHiveSupport().getOrCreate()
+    @classmethod
+    def create_testing_pyspark_session(cls):
+        return SparkSession.builder.master('local').appName('unittest-pyspark').enableHiveSupport().getOrCreate()
 
-	@classmethod
-	def setUpClass(cls):
-		cls.supress_py4j_logging()
-		cls.spark = cls.create_testing_pyspark_session()
+    @classmethod
+    def setUpClass(cls):
+        cls.supress_py4j_logging()
+        cls.spark = cls.create_testing_pyspark_session()
 
-	@classmethod
-	def tearDownClass(cls):
-		cls.spark.stop()
+    @classmethod
+    def tearDownClass(cls):
+        cls.spark.stop()
 
 
 class WikiAggregatorTest(PySparkTest):
-	def test_sanity(self):
-		test_rdd = self.spark.sparkContext.parallelize(['cat dog mouse','cat cat dog'], 2)
-		results = test_rdd.flatMap(lambda line: line.split()).map(lambda word: (word, 1)).reduceByKey(add).collect()
-		expected_results = [('cat', 3), ('dog', 2), ('mouse', 1)]
-		self.assertEqual(set(results), set(expected_results))
+    def test_sanity(self):
+        test_rdd = self.spark.sparkContext.parallelize(['cat dog mouse','cat cat dog'], 2)
+        results = test_rdd.flatMap(lambda line: line.split()).map(lambda word: (word, 1)).reduceByKey(add).collect()
+        expected_results = [('cat', 3), ('dog', 2), ('mouse', 1)]
+        self.assertEqual(set(results), set(expected_results))
 
-	def test_aggregate_domain(self):
-		pv = self.spark.read.csv("pageview-sample", schema=schema, sep = " ")
-		pv.printSchema()
-		print("data types: {}".format(pv.dtypes))
-		pv.show(n=20)
-		pv_count = pv.count()
-		print(f"count pageview {pv_count}")
+    def test_aggregate_domain(self):
+        pv = self.spark.read.csv("pageview-sample", schema=schema, sep = " ")
+        pv.printSchema()
+        print("data types: {}".format(pv.dtypes))
+        pv.show(n=20)
+        pv_count = pv.count()
+        print(f"count pageview {pv_count}")
 
-		clean_pv = clean_pageview(pv)
-		clean_pv.show(n=20)
-		clean_pv_count = clean_pv.count()
-		print(f"count clean pageview {clean_pv_count}")
+        clean_pv = clean_pageview(pv)
+        clean_pv.show(n=20)
+        clean_pv_count = clean_pv.count()
+        print(f"count clean pageview {clean_pv_count}")
 
+        bl = self.spark.read.csv("blacklist", schema=blacklist_schema, sep=" ")
+        bl.show(n=20)
+        print("count blacklist {}".format(bl.count()))
 
-		bl = self.spark.read.csv("blacklist", schema=blacklist_schema, sep=" ")
-		bl.show(n=20)
-		print("count blacklist {}".format(bl.count()))
-		#clean_bl = clean_blacklist(bl)
-		#clean_bl.show(n=20)
+        bl_removed = remove_blacklist(clean_pv, bl)
+        bl_removed.show(n=20)
+        bl_removed_count = bl_removed.count()
+        print(f"count bl removed {bl_removed_count}")
 
-		bl_removed = remove_blacklist(clean_pv, bl)
-		bl_removed.show(n=20)
-		bl_removed_count = bl_removed.count()
-		print(f"count bl removed {bl_removed_count}")
+        transformed = window_and_sort(bl_removed, 3)
+        transformed.show(n=21)
+        transformed_count = transformed.count()
+        print(f"count transformed {transformed_count}")
 
-		transformed = window_and_sort(bl_removed, 3)
-		transformed.show(n=21)
-		transformed_count = transformed.count()
-		print(f"count transformed {transformed_count}")
-
-	"""
-	def test_aggregate_cost(self):
-		fhv = self.spark.read.csv("yellow_tripdata_sample.csv", header=True)
-		fhv.printSchema()
-		print("data types: {}".format(fhv.dtypes))
-		fhv.show(n=20)
-		print("count {}".format(fhv.count()))
-
-		processed = transform_and_aggregate(fhv)
-		print("data types: {}".format(processed.dtypes))
-		processed.show(n=20)
-		print("count {}".format(processed.count()))
-
-		# there are 4 trips going from 264 to 264 in the sample dataset
-
-		# amounts are 12.96, 10.35, 16.8
-		avg_amount = (12.96 + 10.35 + 16.8 + 7.8)/4
-
-		# times are
-		# 2019-02-01 0:15:55	2019-02-01 0:26:26
-		# 2019-02-01 0:31:38	2019-02-01 0:37:59
-		# 2019-02-01 0:42:13	2019-02-01 0:59:37
-		# 2019-02-01 0:45:10	2019-02-01 0:51:12
-		avg_duration = ((10*60)+5+26 + (6*60)+21 + (17*60)+24 + (6*60)+2)/4
-		filtered = processed.filter('key_pair="264-264-2_2019"').collect()
-		self.assertEqual(len(filtered), 1)
-		self.assertEqual(filtered[0]['avg(duration)'], avg_duration)
-		self.assertEqual(filtered[0]['avg(total_amount)'], avg_amount)
-	"""
 
 if __name__ == '__main__':
-	main()
+    main()
